d633fb8d5fd1dfa81b9a68f3319bc3ab6a42638a
dankolar69 <91247767+dankolar69@users.noreply.github.com>
2022-02-10 11:39:30 +0100

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ pins.touch_set_mode(TouchTarget.P2, TouchTargetMode.CAPACITIVE)
 
 start = False
 doba = 0
+pismeno = "S"
 
 def zacatek():
     global start
     doba = randint(3000, 10000)
     basic.pause(doba)
-    basic.show_string("S")
+    basic.show_string(pismeno)
     music.play_tone(Note.C, music.beat(1500))
     start = True
 control.in_background(zacatek)
@@ -18,24 +19,40 @@ def on_forever():
     console.log_value("P1", input.pin_is_pressed(TouchPin.P1))
     console.log_value("P2", input.pin_is_pressed(TouchPin.P2))
 
+    global pismeno
+
     # is_pin1= pins.digitalReadPin(DigitalPin.P1) == 0
     # is_pin2 = pins.digitalReadPin(DigitalPin.P2) == 0
-
-    if input.pin_is_pressed(TouchPin.P1) == True:
-        basic.show_number(1)
-        basic.pause(5000)
-        control.reset()
-    if input.pin_is_pressed(TouchPin.P2) == True:
-        basic.show_number(2)
-        basic.pause(5000)
-        control.reset()
-    if input.pin_is_pressed(TouchPin.P1) and input.pin_is_pressed(TouchPin.P2) == True:
-        basic.show_string("R")
-        basic.pause(5000)
-        control.reset()
-
     
     
+    if start == True:
+        if input.pin_is_pressed(TouchPin.P1) == True:
+            basic.show_number(1)
+            basic.pause(5000)
+            control.reset()
+        if input.pin_is_pressed(TouchPin.P2) == True:
+            basic.show_number(2)
+            basic.pause(5000)
+            control.reset()
+        if input.pin_is_pressed(TouchPin.P1) and input.pin_is_pressed(TouchPin.P2) == True:
+            basic.show_string("R")
+            basic.pause(5000)
+            control.reset()
+    
+    if start == False:
+        if  input.pin_is_pressed(TouchPin.P1) == True:
+            pismeno = "B" 
+            basic.pause(5000)
+            control.reset()
+        if input.pin_is_pressed(TouchPin.P2) == True:
+            pismeno = "A"
+            basic.pause(5000)
+            control.reset()
+        if input.pin_is_pressed(TouchPin.P1) and input.pin_is_pressed(TouchPin.P2) == True:
+            pismeno = "C"
+            basic.pause(5000)
+            control.reset()
+
 
 basic.forever(on_forever)
 
--- main.ts
+++ main.ts
@@ -2,35 +2,61 @@ pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
 pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
 let start = false
 let doba = 0
+let pismeno = "S"
 control.inBackground(function zacatek() {
     
     let doba = randint(3000, 10000)
     basic.pause(doba)
-    basic.showString("S")
+    basic.showString(pismeno)
     music.playTone(Note.C, music.beat(1500))
     start = true
 })
 basic.forever(function on_forever() {
     console.logValue("P1", input.pinIsPressed(TouchPin.P1))
     console.logValue("P2", input.pinIsPressed(TouchPin.P2))
+    
     //  is_pin1= pins.digitalReadPin(DigitalPin.P1) == 0
     //  is_pin2 = pins.digitalReadPin(DigitalPin.P2) == 0
-    if (input.pinIsPressed(TouchPin.P1) == true) {
-        basic.showNumber(1)
-        basic.pause(5000)
-        control.reset()
+    if (start == true) {
+        if (input.pinIsPressed(TouchPin.P1) == true) {
+            basic.showNumber(1)
+            basic.pause(5000)
+            control.reset()
+        }
+        
+        if (input.pinIsPressed(TouchPin.P2) == true) {
+            basic.showNumber(2)
+            basic.pause(5000)
+            control.reset()
+        }
+        
+        if (input.pinIsPressed(TouchPin.P1) && input.pinIsPressed(TouchPin.P2) == true) {
+            basic.showString("R")
+            basic.pause(5000)
+            control.reset()
+        }
+        
     }
     
-    if (input.pinIsPressed(TouchPin.P2) == true) {
-        basic.showNumber(2)
-        basic.pause(5000)
-        control.reset()
-    }
-    
-    if (input.pinIsPressed(TouchPin.P1) && input.pinIsPressed(TouchPin.P2) == true) {
-        basic.showString("R")
-        basic.pause(5000)
-        control.reset()
+    if (start == false) {
+        if (input.pinIsPressed(TouchPin.P1) == true) {
+            pismeno = "B"
+            basic.pause(5000)
+            control.reset()
+        }
+        
+        if (input.pinIsPressed(TouchPin.P2) == true) {
+            pismeno = "A"
+            basic.pause(5000)
+            control.reset()
+        }
+        
+        if (input.pinIsPressed(TouchPin.P1) && input.pinIsPressed(TouchPin.P2) == true) {
+            pismeno = "C"
+            basic.pause(5000)
+            control.reset()
+        }
+        
     }
     
 })
